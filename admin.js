8cf0950b819e5d31ae6f7a65bfde15534daf900a
Shipra-zzz <97395310+Shipra-zzz@users.noreply.github.com>
2023-11-25 09:07:29 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -8,7 +8,7 @@ const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567
 // Define the length of the random link
 const linkLength = 10;
 
-// Function to generate a random link
+// Function to generate a random link for the gaming sessions
 function generateRandomLink() {
   let randomLink = '';
   for (let i = 0; i < linkLength; i++) {
@@ -28,6 +28,7 @@ generateLinkButton.addEventListener('click', function () {
 // Initial text for the invite link
 inviteLinkElement.textContent = 'Your Invite Link Will Appear Here';
 
+// Starting the game 
 let start=document.getElementById('start-game-button');
 start.addEventListener("click", function () {
     window.location.href = "game.html";
